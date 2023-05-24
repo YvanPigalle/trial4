e57ac10a129b798866db3760d2093b9575c7845e
YvanPigalle <vanquishracer@gmail.com>
2023-05-24 22:28:45 +0300

    3of3done

--- Program.cs
+++ Program.cs
@@ -41,3 +41,31 @@ sum = sum + num;
 Console.WriteLine("The sum of all digits of this number: " + sum);
 
 */
+
+
+
+//Задача: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+int [] Create8Array (int size, int minVal, int maxVal)
+{
+    int [] newArray = new int [size];
+    for (int i = 0; i < size; i++)
+    newArray[i] = new Random().Next(minVal, maxVal + 1);
+    return newArray;
+}
+
+void  ShowArray (int [] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    Console.Write (array1[i]+ " ");
+    Console.WriteLine();
+}
+
+int sizeArray = 8;
+int minVal = -999;
+int maxVal = 999;
+
+int [] createdArray = Create8Array(sizeArray, minVal, maxVal);
+ShowArray(createdArray);
