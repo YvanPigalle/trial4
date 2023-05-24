58f17e0bb291760d4e04c8acbd457d9ba2cf0e86
YvanPigalle <vanquishracer@gmail.com>
2023-05-24 20:15:57 +0300

    2of3done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,43 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//Задача: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
 
+
+
+/*
+
+Console.Write("Input A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input B ");
+int b = Convert.ToInt32(Console.ReadLine());
+int pwr = a;
+
+for (int i = 1; i < b; i++)
+{
+pwr *= a;
+}
+Console.WriteLine("Power of A for B is: " + pwr);
+
+*/
+
+
+
+
+
+//Задача: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+/*
+
+Console.Write("Input number: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("The sum of all digits of this number: " + sum);
+
+*/
